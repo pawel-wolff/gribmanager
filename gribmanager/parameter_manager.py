3eb29ff4ca3b56b5fb947f95c8a48e81a5c645ab
Pawel Wolff <pawel.wolff@aero.obs-mip.fr>
2019-12-02 16:46:39 +0100

    repr added + name refactoring

--- gribmanager/parameter_manager.py
+++ gribmanager/parameter_manager.py
@@ -6,8 +6,14 @@ import inspect
 import pandas as pd
 
 
-class HorizontalParameter:
+class Parameter:
+    def __repr__(self):
+        return repr(type(self))
+
+
+class HorizontalParameter(Parameter):
     def __init__(self, parameter):
+        super().__init__()
         self._parameter = parameter
 
     def get_value_at(self, lat, lon, pressure=None):
@@ -16,9 +22,18 @@ class HorizontalParameter:
     def another_get_value_at(self, lat, lon, pressure=None):
         return self.get_value_at(lat, lon)
 
+    def __repr__(self):
+        dump = [super().__repr__()]
+        dump.append(f'2d parameter: {self._parameter.get(gk.SHORT_NAME)} - {self._parameter.get(gk.NAME)} '
+                    f'(parameter id={self._parameter.get(gk.PARAMETER_ID)})')
+        dump.append(f'\tlevel: {self._parameter.get(gk.LEVEL)}')
+        dump.append(f'\ttype of level: {self._parameter.get(gk.TYPE_OF_LEVEL)}')
+        return '\n'.join(dump)
 
-class VerticalParameter:
+
+class VerticalParameter(Parameter):
     def __init__(self, parameter_at_all_levels):
+        super().__init__()
         self._parameter_at_all_levels = sorted(parameter_at_all_levels, key=lambda grib_message: grib_message[gk.LEVEL])
         self._level = [grib_message[gk.LEVEL] for grib_message in self._parameter_at_all_levels]
         self._no_levels = len(self._level)
@@ -67,6 +82,15 @@ class VerticalParameter:
 
         return root_node.interpolate((lat, lon, pressure))
 
+    def __repr__(self):
+        dump = [super().__repr__()]
+        parameter_at_any_level = self._parameter_at_all_levels[0]
+        dump.append(f'3d parameter: {parameter_at_any_level.get(gk.SHORT_NAME)} - {parameter_at_any_level.get(gk.NAME)} '
+                    f'(parameter id={parameter_at_any_level.get(gk.PARAMETER_ID)})')
+        dump.append(f'\tnumber of available levels: {self._no_levels}')
+        dump.append(f'\tavailable levels: {self._level}')
+        return '\n'.join(dump)
+
 
 class VerticalParameterInModelLevel(VerticalParameter):
     # TODO: manage failure cases
@@ -78,33 +102,41 @@ class VerticalParameterInModelLevel(VerticalParameter):
         """
         super().__init__(parameter_at_all_levels)
         self._surface_pressure = surface_pressure
-        self._a, self._b = self._get_model_level_definition_coefficients()
+        self._no_model_levels, self._a, self._b = self._get_model_level_definitions()
 
     def _get_pressure_of_model_level(self, surface_pressure_at_location, index):
         level = self._level[index]
         # self._a[level] + self._b[level] * surface_pressure_at_location
         # is the pressure at a half-level (the interface between level layers)
         # and we need the pressure at the full-level
-        return 0.5 * (self._a[level] + self._a[level - 1]) + 0.5 * (self._b[level] + self._b[level - 1]) * surface_pressure_at_location
+        return 0.5 * (self._a[level] + self._a[level - 1]) + \
+               0.5 * (self._b[level] + self._b[level - 1]) * surface_pressure_at_location
 
     def _pressure_of_all_levels(self, lat, lon):
         surface_pressure = self._surface_pressure.get_value_at(lat, lon)
         return (self._get_pressure_of_model_level(surface_pressure, i) for i in range(self._no_levels))
 
-    # TODO: can be optimized with binary search; look at profiling
     def _index_and_pressure_of_sandwiching_levels(self, pressure, lat, lon):
         surface_pressure = self._surface_pressure.get_value_at(lat, lon)
         return utils.sandwiching_values_by_binary_search(pressure, 0, self._no_levels-1,
                                                          functools.partial(self._get_pressure_of_model_level, surface_pressure), aux=(lat, lon))
 
-    def _get_model_level_definition_coefficients(self):
+    def _get_model_level_definitions(self):
         level_definition_coefficients = self._parameter_at_all_levels[0][gk.PV]
-        n_levels = len(level_definition_coefficients) // 2 - 1
-        a_in_Pa = level_definition_coefficients[:(n_levels + 1)]
-        b_coeff = level_definition_coefficients[(n_levels + 1):]
-        return a_in_Pa, b_coeff
+        no_model_levels = len(level_definition_coefficients) // 2 - 1
+        a_in_Pa = level_definition_coefficients[:(no_model_levels + 1)]
+        b_coeff = level_definition_coefficients[(no_model_levels + 1):]
+        return no_model_levels, a_in_Pa, b_coeff
+
+    def __repr__(self):
+        dump = [super().__repr__()]
+        parameter_at_any_level = self._parameter_at_all_levels[0]
+        dump.append(f'\ttype of levels: {parameter_at_any_level.get(gk.TYPE_OF_LEVEL)}')
+        dump.append(f'\tnumber of model levels: {self._no_model_levels}')
+        return '\n'.join(dump)
 
 
+# TODO: manage case when at some levels are in hPa and others in Pa; must fix super().__init__ in that respect (sorting wrt level)
 class VerticalParameterInPressureLevel(VerticalParameter):
     def __init__(self, parameter_at_all_levels):
         super().__init__(parameter_at_all_levels)
@@ -118,12 +150,19 @@ class VerticalParameterInPressureLevel(VerticalParameter):
         return utils.sandwiching_values_by_binary_search(pressure, 0, self._no_levels-1,
                                                          lambda index: self._pressure_by_level[index], aux=(lat, lon))
 
+    def __repr__(self):
+        dump = [super().__repr__()]
+        parameter_at_any_level = self._parameter_at_all_levels[0]
+        dump.append(f'\ttype of levels: {parameter_at_any_level.get(gk.TYPE_OF_LEVEL)}')
+        return '\n'.join(dump)
+
 
 class ParameterManager:
     _INDEXING_KEYS = [gk.PARAMETER_ID]
 
     def __init__(self, grib_filename):
-        self._grib_file_indexed = gm.GribFileIndexedByWithCache(grib_filename, *ParameterManager._INDEXING_KEYS)
+        self._grib_filename = grib_filename
+        self._grib_file_indexed = gm.GribFileIndexedByWithCache(self._grib_filename, *ParameterManager._INDEXING_KEYS)
 
     def get_parameter(self, param_id, predicate=None, must_be_unique=False):
         def arguments_to_string():
@@ -134,7 +173,8 @@ class ParameterManager:
                     predicate_source = '<source code not available>'
             else:
                 predicate_source = 'None'
-            return f'GRIB file={str(self._grib_file_indexed)}, param_id={param_id}, predicate={predicate_source}, must_be_unique={must_be_unique}'
+            return f'GRIB file={str(self._grib_file_indexed)}, param_id={param_id}, predicate={predicate_source}, ' \
+                   f'must_be_unique={must_be_unique}'
 
         parameters = self._grib_file_indexed[param_id]
         if predicate is not None:
@@ -150,9 +190,15 @@ class ParameterManager:
             elif all(parameter.is_level_isobaric() for parameter in parameters):
                 return VerticalParameterInPressureLevel(parameters)
             else:
-                raise ValueError(f'{arguments_to_string()}: not a vertical parameter (neither it is in model level, nor in pressure level) or an unknown vertical parameter')
+                raise ValueError(f'{arguments_to_string()}: not a vertical parameter (neither it is in model level, '
+                                 f'nor in pressure level) or an unknown vertical parameter')
         elif len(parameters) == 1:
             # horizontal (2d) parameter
             return HorizontalParameter(parameters[0])
         else:
             raise ValueError(f'{arguments_to_string()}: no such parameters found')
+
+    def __repr__(self):
+        dump = [repr(type(self))]
+        dump.append(f'GRIB file {self._grib_filename}')
+        return '\n'.join(dump)
